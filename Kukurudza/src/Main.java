b99d41f54b2278c17cfe63b63fb2223e26a10cd9
SerhiiKukurudza <sironya.1204@gmail.com>
2022-12-13 00:26:21 +0200

    my second commit

--- Kukurudza/src/Main.java
+++ Kukurudza/src/Main.java
@@ -1,6 +1,7 @@
 import course_1.lecture.lec.lec.Lec1;
 import course_1.lecture.lec.lec.Lec2;
 import course_1.lecture.lec.lec.Lec3;
+import course_1.lecture.lec.lec.Lec4;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +9,7 @@ public class Main {
         Lec1 lec1 = new Lec1();
         Lec2 lec2 = new Lec2();
         Lec3 lec3 = new Lec3();
+        Lec4 lec4 = new Lec4();
 
     }
 }
