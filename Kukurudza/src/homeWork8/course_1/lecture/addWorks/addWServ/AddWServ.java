fdac699a7d792a12ecc340f68e68a2891b4b5d41
SerhiiKukurudza <sironya.1204@gmail.com>
2022-12-26 23:37:03 +0200

    homework 8

--- Kukurudza/src/homeWork8/course_1/lecture/addWorks/addWServ/AddWServ.java
+++ Kukurudza/src/homeWork8/course_1/lecture/addWorks/addWServ/AddWServ.java
@@ -2,15 +2,11 @@ package homeWork8.course_1.lecture.addWorks.addWServ;
 
 import homeWork8.course_1.lecture.addWorks.addWorks.AddW1;
 
-
-public class AddWSr {
-
+public class AddWServ {
     public void addW() {
 
         AddW1 addW1 = new AddW1();
         int num = addW1.NUM;
         System.out.println("Add works №" + num);
-
     }
 }
-
