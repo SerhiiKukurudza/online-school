59d15a264c0228e69c92bfa571a836dbc34694ab
SerhiiKukurudza <sironya.1204@gmail.com>
2023-01-08 10:16:04 +0200

    delete

--- Kukurudza/src/homeWork15/course_1/student/student/Student.java
+++
@@ -1,7 +0,0 @@
-package homeWork15.course_1.student.student;
-
-public class Student {
-
-    private int id = 1;
-
-}
--- Kukurudza/src/homeWork15/course_1/teacher/teacher/DataTeacher.java
+++
@@ -1,5 +0,0 @@
-package homeWork15.course_1.teacher.teacher;
-
-public class DataTeacher {
-    public static Teacher[] teachers = new Teacher[2];
-}
